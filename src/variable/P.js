23ce2d46e5b8f09544448d84770f370a9962ec3d
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2025-01-12 18:05:04 +0800

    feat: table range preview

--- src/variable/P.js
+++ src/variable/P.js
@@ -1,7 +1,11 @@
 import { ParseVariable } from "./index.js";
+import { translate } from "../mode/index.js";
 
 export const ParseTableRange = (parameter) => {
-  const rangeIndex = ['Start', 'End', 'Step'];
+  const rangeIndex = translate({
+    'en': ['Start', 'End', 'Step'],
+    'zh': ['开始值', '终止值', '步长'],
+  });
   const split = parameter.match(/.{9}/g);
   const result = [];
   for (let i = 0; i < split.length; i++) {

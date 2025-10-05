6f5f02c329119c683e6ebd4188f6b819afb22946
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2025-10-05 15:03:08 +0800

    refactor

--- test/ascii.js
+++ test/ascii.js
@@ -1,4 +1,19 @@
 import { AsciiTable } from '../src/ascii/index.js';
 
-// console.log(new AsciiTable('EY','001').get())
-console.log(new AsciiTable('EY','001').get('unicode'))
+let unicode, latex;
+
+unicode = (new AsciiTable('EY', '001').get('unicode'))
+latex = (new AsciiTable('EY', '001').get('latex'))
+console.log(unicode, latex)
+
+unicode = (new AsciiTable('EY', '031').get('unicode'))
+latex = (new AsciiTable('EY', '031').get('latex'))
+console.log(unicode, latex)
+
+unicode = (new AsciiTable('CY', '239').get('unicode'))
+latex = (new AsciiTable('CY', '239').get('latex'))
+console.log(unicode, latex)
+
+unicode = (new AsciiTable('CY', '243').get('unicode'))
+latex = (new AsciiTable('CY', '243').get('latex'))
+console.log(unicode, latex)

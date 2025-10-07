15f21e2128a7fd7df9a1b3bd4589bd39c966f974
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2025-10-07 15:48:44 +0800

    refactor: i18n `table.range`

--- src/variable/P.js
+++ src/variable/P.js
@@ -1,16 +1,12 @@
 import { ParseVariable } from "./index.js";
-import { translate } from "../utils.js";
+import { tt } from "../utils.js";
 
 export const ParseTableRange = (parameter) => {
-  const rangeIndex = translate({
-    'en': ['Start', 'End', 'Step'],
-    'zh': ['开始值', '终止值', '步长'],
-  });
   const split = parameter.match(/.{9}/g);
   const result = [];
   for (let i = 0; i < split.length; i++) {
     const [latex, decimal] = new ParseVariable(`0${split[i]}`).get();
-    result.push({ name: rangeIndex[i], latex, decimal });
+    result.push({ name: tt(`input.TABLE.${i}`), latex, decimal });
   }
   return result;
 }

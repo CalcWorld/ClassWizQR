61dfcb96390b8c98988c5a8b0709871d27df53a9
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2025-10-12 14:11:42 +0800

    chore

--- src/algo/index.js
+++ src/algo/index.js
@@ -10,7 +10,7 @@ export class ParseAlgorithm {
    * @param {string} modelId
    */
   constructor(S, E, modelType, modelId) {
-    this.unitSetiing = new ParseSetup(S || '').getAlgorithmUnitSettingCode() || '0'; // default to 0-pixels
+    this.unitSetiing = new ParseSetup(S || '').getAlgorithmUnitSettingCode() || '0';
     this.E = E;
     const asciiTable = new AsciiTable(modelType, modelId);
     this.asciiLatexTable = asciiTable.get('latex');

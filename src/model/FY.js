0b906c84a5639d3ffb7f5e933a7e8945212be97e
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2026-01-08 20:26:13 +0800

    add models

--- src/model/FY.js
+++ src/model/FY.js
@@ -1,4 +1,8 @@
 export const modelFY = {
   "505": ["fx-9910CW 2nd edition", 2],
   "506": ["fx-92+ Collège", 2],
+  "521": ["fx-82NL+", 2],
+  "540": ["fx-83GT CW+ 2nd edition", 2],
+  "541": ["fx-85GT CW+ 2nd edition", 2],
+  "542": ["fx-991CW+ UK 2nd edition", 2],
 };

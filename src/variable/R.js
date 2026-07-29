0c82e26bc73c546080d6717485c47458515f7da9
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2026-07-29 10:35:01 +0800

    fix: imaginary part bracket

--- src/variable/R.js
+++ src/variable/R.js
@@ -15,7 +15,7 @@ export const ParseNumberResult = ({ R, M }, { modelType, modelId } = {}) => {
   let template = RESULT_INFO['NUMBER'][parseM.getResultTemplate()];
   let result = [];
   if (!template) {
-    const ans2LatexBracket = /[+-]/.test(ans2Latex) ? `(${ans2Latex})` : ans2Latex;
+    const ans2LatexBracket =  /^.+[+-]/.test(ans2Latex) ? `(${ans2Latex})` : ans2Latex;
     const real_is_zero = ans1Decimal.isZero();
     const imaginary_is_zero = ans2Decimal.isZero();
 
